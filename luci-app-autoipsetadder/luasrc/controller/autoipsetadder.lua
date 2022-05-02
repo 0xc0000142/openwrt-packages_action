cdf4c210510b7e9d4e205796cea640272ee8ae6c
actions <action@github.com>
2022-05-02 20:44:45 +0800

    update@20220502

--- luci-app-autoipsetadder/luasrc/controller/autoipsetadder.lua
+++ luci-app-autoipsetadder/luasrc/controller/autoipsetadder.lua
@@ -4,10 +4,7 @@ local fs=require"nixio.fs"
 local sys=require"luci.sys"
 local uci=require"luci.model.uci".cursor()
 function index()
-	local page = entry({"admin","services","autoipsetadder"},firstchild(),_("autoipsetadder"))
-	page.order = 30
-	page.dependent = true
-	page.acl_depends = { "luci-app-autoipsetadder" }
+	entry({"admin","services","autoipsetadder"},firstchild(),_("autoipsetadder"),30).dependent=true
 	entry({"admin","services","autoipsetadder","autoipsetadder"},cbi("autoipsetadder"),_("Base Setting"),1)
     entry({"admin","services","autoipsetadder","status"},call("act_status")).leaf=true
 	entry({"admin", "services", "autoipsetadder", "getlog"}, call("get_log"))
