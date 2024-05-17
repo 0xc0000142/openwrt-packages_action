5d961c2ea7cede2afa575d3dd9aa6abf4a8e7f97
0xc0000142 <52188337+0xc0000142@users.noreply.github.com>
2024-05-17 23:39:07 +0800

    fix: wrong clone dir

--- packages-21.02.sh
+++ packages-21.02.sh
@@ -8,8 +8,8 @@
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/
 #svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/UnblockNeteaseMusic ./UnblockNeteaseMusic-Go
 #svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/luci-app-unblockneteasemusic ./luci-app-UnblockNeteaseMusicGo
-git clone --depth 1 https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/luci-app-unblockneteasemusic ./luci-app-UnblockNeteaseMusicGo 
-git clone --depth 1  https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/UnblockNeteaseMusic ./UnblockNeteaseMusic-Go 
+git clone --depth 1 https://github.com/cnsilvan/luci-app-unblockneteasemusic/ ./luci-app-UnblockNeteaseMusicGo 
+git clone --depth 1  https://github.com/cnsilvan/luci-app-unblockneteasemusic ./UnblockNeteaseMusic-Go 
 
 git clone --depth 1 https://github.com/kenzok8/small-package.git
 git clone --depth 1 https://github.com/Lienol/openwrt-package 
