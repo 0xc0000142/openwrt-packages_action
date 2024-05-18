31f98fbb9f36994122b5251fcdf9dbedfca7c462
0xc0000142 <52188337+0xc0000142@users.noreply.github.com>
2024-05-18 14:34:21 +0800

    fix: wrong url

--- packages-21.02.sh
+++ packages-21.02.sh
@@ -13,6 +13,7 @@ git clone --depth 1  https://github.com/cnsilvan/luci-app-unblockneteasemusic ./
 
 git clone --depth 1 https://github.com/kenzok8/small-package.git ./package48748641
 git clone --depth 1 https://github.com/Lienol/openwrt-package ./package136461651
+git clone --depth 1 https://github.com/obsy/packages/ ./package165456316486
 git clone --depth 1 https://github.com/0xc0000142/openwrt-r8168.git
 git clone --depth 1 https://github.com/fw876/helloworld
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall 
@@ -37,34 +38,30 @@ git clone --depth 1 https://github.com/rufengsuixing/luci-app-onliner
 git clone --depth 1 https://github.com/tty228/luci-app-serverchan 
 git clone --depth 1 https://github.com/4IceG/luci-app-sms-tool smstool 
 git clone --depth 1 https://github.com/rufengsuixing/luci-app-usb3disable 
-git clone --depth 1 https://github.com/fszok/luci-app-advancedsetting.git
+git clone --depth 1 https://github.com/alvazhang79/luci-app-advancedsetting
 git clone --depth 1 https://github.com/silime/luci-app-xunlei 
 git clone --depth 1 https://github.com/ysc3839/luci-proto-minieap 
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot 
 git clone --depth 1 https://github.com/shanglanxin/luci-app-homebridge 
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config 
-git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr 
-git clone --depth 1 https://github.com/jerrykuku/luci-app-ttnode ttnode && mv -n ttnode/ttnode/* ./ ; rm -Rf ttnode 
+#git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr 
+git clone --depth 1 https://github.com/jerrykuku/luci-app-ttnode 
 git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
 git clone --depth 1 https://github.com/sirpdboy/luci-theme-kucat
-git clone --depth 1 https://github.com/sirpdboy/luci-theme-parentcontrol
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus
 git clone --depth 1 https://github.com/sirpdboy/luci-app-wizard
 git clone --depth 1 https://github.com/sirpdboy/luci-app-partexp
-git clone --depth 1 https://github.com/sirpdboy/luci-app-lukcy
 
 git clone --depth 1 https://github.com/jefferymvp/luci-app-koolproxyR 
 git clone --depth 1 https://github.com/peter-tank/luci-app-dnscrypt-proxy2 
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced 
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata 
-git clone --depth 1 https://github.com/jerrykuku/luci-app-jd-dailybonus 
+git clone --depth 1 https://github.com/noiver/luci-app-jd-dailybonus
 git clone --depth 1 https://github.com/NateLol/luci-app-oled 
 git clone --depth 1 https://github.com/destan19/OpenAppFilter
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff 
 git clone --depth 1 https://github.com/lvqier/luci-app-dnsmasq-ipset 
-git clone --depth 1 https://github.com/small-5/ddns-scripts-dnspod 
-git clone --depth 1 https://github.com/small-5/ddns-scripts-aliyun 
 git clone --depth 1 https://github.com/walkingsky/luci-wifidog luci-app-wifidog 
 git clone --depth 1 https://github.com/peter-tank/luci-app-autorepeater 
 git clone --depth 1 https://github.com/CCnut/feed-netkeeper 
@@ -76,19 +73,19 @@ git clone --depth 1 https://github.com/LGA1150/openwrt-sysuh3c
 git clone --depth 1 https://github.com/gdck/luci-app-cupsd cupsd1 && mv -n cupsd1/luci-app-cupsd cupsd1/cups/cups ./ ; rm -rf cupsd1 
 git clone --depth 1 https://github.com/4IceG/luci-app-socat.git
 
-git clone --depth 1 https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns/tencentcloud_ddns luci-app-tencentddns 
-git clone --depth 1 https://github.com/riverscn/openwrt-iptvhelper/iptvhelper
-git clone --depth 1 https://github.com/riverscn/openwrt-iptvhelper/luci-app-iptvhelper
+git clone --depth 1 https://github.com/msylgj/luci-app-tencentddns
+git clone --depth 1 https://github.com/riverscn/openwrt-iptvhelper/
 git clone --depth 1 https://github.com/brvphoenix/wrtbwmon/wrtbwmon
 git clone --depth 1 https://github.com/KFERMercer/luci-app-tcpdump ./luci-app-tcpdump
 git clone --depth 1 https://github.com/tty228/luci-app-nodogsplash ./luci-app-nodogsplash
-git clone --depth 1 https://github.com/koshev-msk/luci-app-atinout ./luci-app-atinout
+git clone --depth 1 https://github.com/SankySyS/luci-app-atinout ./luci-app-atinout
+git clone --depth 1 https://github.com/4IceG/luci-app-atinout-mod
+git clone --depth 1 https://github.com/izilzty/luci-app-chinadns-ng
 cp -r ./luci-app-atinout/atinout ./
-git clone --depth 1 https://github.com/muink/luci-app-pcap-dnsproxy/branches/dev ./luci-app-pcap-dnsproxy
-git clone --depth 1 https://github.com/koshev-msk/3proxy-openwrt ./3proxy
-git clone --depth 1 https://github.com/pexcn/openwrt-chinadns-ng/branches/luci ./luci-app-chinadns-ng
-git clone --depth 1 https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/udpspeeder
-git clone --depth 1 https://github.com/obsy/packages/3ginfo
+
+git clone --depth 1 https://github.com/muink/luci-app-pcap-dnsproxy/./luci-app-pcap-dnsproxy
+git clone --depth 1 https://github.com/osnosn/3proxy-openwrt ./3proxy
+
 git clone --depth 1 https://github.com/lisaac/luci-app-diskman ./luci-app-diskman
 mkdir parted
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O parted/Makefile
@@ -101,8 +98,9 @@ git clone --depth 1 https://github.com/cnzd/luci-app-koolproxyR ./luci-app-koolp
 git clone --depth 1 https://github.com/MiRouter/luci-app-vssr-plus ./luci-app-vssr-plus
 git clone --depth 1 https://github.com/jerrykuku/node-request ./node-request
 git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb ./lua-maxminddb
-git clone --depth 1 https://github.com/garypang13/luci-app-eqos ./luci-app-eqos
-git clone --depth 1 https://github.com/jerrykuku/luci-app-jd-dailybonus ./luci-app-jd-dailybonus
+git clone --depth 1 https://github.com/Huangjoe123/luci-app-eqos ./luci-app-eqos
+git clone --depth 1 https://github.com/sirpdboy/luci-app-eqosplus
+git clone --depth 1 https://github.com/noiver/luci-app-jd-dailybonus ./luci-app-jd-dailybonus
 #git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr ./luci-app-vssr
 git clone --depth 1 https://github.com/tty228/luci-app-serverchan ./luci-app-serverchan
 git clone --depth 1 https://github.com/frainzy1477/luci-app-clash ./luci-app-clash
@@ -146,9 +144,11 @@ done
 
 mv package48748641/* ./ -n
 mv package136461651/* ./ -n
+mv package165456316486/* ./ -n
 rm -rf ./*/.git ./*/.gitattributes ./*/.svn ./*/.github ./*/.gitignore README.md ./*/README.md ./*/*/README.md
 rm -rf ./package48748641 ./package136461651
 
+mkdir r8168
 mv -f ./openwrt-r8168/* ./r8168/
 rm -rf openwrt-r8168
 rm -rf ./*/.git
